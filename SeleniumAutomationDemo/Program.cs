f4471bf39ae7d540d32a39c184476dbefac43d3b
Vitor Gabriel Melo Silva <vitorextre@gmail.com>
2026-04-09 21:58:22 -0300

    pacotes  e usings

--- SeleniumAutomationDemo/Program.cs
+++ SeleniumAutomationDemo/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+class Program
+{
+    static void Main(string [] args){
+      
+    }
+}
